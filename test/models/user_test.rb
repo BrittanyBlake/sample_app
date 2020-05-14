56e2c493bdd4c95bcb54fb858acde74fc05caa3e
Brittany Blake <brittany.blake49@gmail.com>
2020-05-14 15:42:52 +0200

    uniqueness validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -49,7 +49,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email addresses should be unique" do
-    duplicate_user = @user.duplicate_user
+    duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
   end
